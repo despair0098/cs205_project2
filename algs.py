875db33520e737ee6d37dd6b7ae10ea1ec5089d2
despair0098 <zijunwang324@gmail.com>
2025-05-25 17:42:28 -0700

    Coded the NN algorithm

--- algs.py
+++ algs.py
@@ -1,16 +1,34 @@
 import copy
 import math
 import time
+import numpy as np
+from collections import Counter
 
-
+# data = X_train, features = Y_train, cur = the current point inside X_test
 
 def KFold(data, k):
    
 
    return 0 
 
+def euclidean(point1, point2, features):
+   distance = 0
+   for feature in features:
+      distance += (point1[feature] - point2[feature]) ** 2
+   return np.sqrt(distance)
+
 def NearestNeighbor(data, features, curr):
-   return 0
+    neighborDist = math.inf
+    nearest = 0
+    for i in range(len(data)):
+        if i != curr:
+            distance = euclidean(data[curr], data[i], features)
+            if distance <= neighborDist:
+               # Set nearest neighbor to current data point
+               neighborDist = distance
+               # it stores the class label of the nearest data point
+               nearest = data[i][0] 
+    return nearest                            
 
 def forwardSelection(data, features):
    return 0
